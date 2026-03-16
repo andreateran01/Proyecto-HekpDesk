9e43848c2186b53b33d4e516743cd3fa61336d5d
Andrea de la Paz Cortes Teran <andreateran7459@gmail.com>
2026-03-16 03:32:39 -0600

    Primer commit

---
+++ clases/Conexion.php
@@ -0,0 +1,12 @@
+<?php
+    class Conexion{
+        public function conectar(){
+            $servidor = "localhost";
+            $usuario = "root";
+            $password = "";
+            $db = "helpdesk";
+        }
+    }
+
+
+?>
