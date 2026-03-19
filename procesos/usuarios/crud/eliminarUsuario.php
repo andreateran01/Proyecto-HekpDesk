d5d28402a875c221926d3cc90945fca95386c12e
Andrea de la Paz Cortes Teran <andreateran7459@gmail.com>
2026-03-18 23:42:21 -0600

    En este se suben  modificiones para que funcionen

--- procesos/usuarios/crud/eliminarUsuario.php
+++ procesos/usuarios/crud/eliminarUsuario.php
@@ -1,12 +1,19 @@
 <?php
 
-    include "../../../clases/Usuarios.php";
+include "../../../clases/Usuarios.php";
+
+if(isset($_POST['idUsuario']) && isset($_POST['idPersona'])){
+
     $Usuarios = new Usuarios();
+
     $datos = array(
         "idUsuario" => $_POST['idUsuario'],
         "idPersona" => $_POST['idPersona']
     );
-    
+
     echo $Usuarios->eliminarUsuario($datos);
 
+} else {
+    echo "Error: datos incompletos";
+}
 ?>
